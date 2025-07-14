bd25bbba417bb48b66f6edf3fe18d0a9aec61a49
sehamanter1 <sehamanter2@gmail.com>
2025-07-14 20:36:32 +0300

    groupping problem

---
+++ P1.js/p1_groupingByKey.js
@@ -0,0 +1,37 @@
+// Problem 1: Group by Property
+// Prompt:
+// Write a function that takes an array of objects and returns an object grouping them by a given key.
+// const input = [
+//   { name: 'Alice', role: 'dev' },
+//   { name: 'Bob', role: 'designer' },
+//   { name: 'Charlie', role: 'dev' }
+// ];
+
+// groupBy(input, 'role');
+// // Expected output:
+// // {
+// //   dev: [
+// //     { name: 'Alice', role: 'dev' },
+// //     { name: 'Charlie', role: 'dev' }
+// //   ],
+// //   designer: [
+// //     { name: 'Bob', role: 'designer' }
+// //   ]
+// // }
+//******* sol : *******//
+const input = [];
+
+function groupBy(array,key){
+  return array.reduce((result,currentItem)=>{
+    const groupItem=currentItem[key];
+    if(!result[groupItem]){
+      result[groupItem]=[]
+    }
+     result[groupItem].push(currentItem)
+    return result
+  },{})
+
+}
+let groupedData = groupBy(input, 'role');
+
+console.log(groupedData)
