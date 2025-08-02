25a024b15d70745e818c4af035612325c3948c99
sehamanter1 <sehamanter2@gmail.com>
2025-08-03 00:42:12 +0300

    remove logs

--- P4.js
+++ P4.js
@@ -6,25 +6,13 @@
 var join = function(arr1, arr2) {
     let joinedArray,result=[];
     joinedArray=[...arr1,...arr2];
-                console.log("item",joinedArray)
-//  res: { id: 1, x: 5, y: 10 },{ id: 1, x: 0, z: 3 },
-//  res:  { id: 2, x: 7 }, 
-//  res:{ id: 3, x: 9 }
-
-                joinedArray.forEach(item=>{
+        joinedArray.forEach(item=>{
         if(Array.isArray(result)&&result.length&&result.find((obj)=>obj.id===item.id)){
             const index = result.findIndex((obj)=>obj.id===item.id);
-                console.log("item[index]",result[index])
-                console.log("item",item)
             Object.entries(result[index]).forEach(([key,value])=>{
-                console.log("keu:value",key,value)
-                console.log("result.key",result[index][key])
                 if(!(key in item)){
-                    console.log("item key not found",key)
                     item[key]=result[index][key]
-                }
-                
-                //  item.key=value
+                }                
             })
             result[index]=item;
         }else{
