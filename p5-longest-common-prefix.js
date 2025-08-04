bd0350fe02b28961a9a5825179d88d7d50854160
sehamanter1 <sehamanter2@gmail.com>
2025-08-04 05:34:28 +0300

    p5- remove logs

--- p5-longest-common-prefix.js
+++ p5-longest-common-prefix.js
@@ -4,33 +4,20 @@
  */
 // Write a function to find the longest common prefix string amongst an array of strings.
 // If there is no common prefix, return an empty string "".
-var longestCommonPrefix = function(strs) {
-    let ans=strs[0],res="",res2="";
-    for (let i =1; i<strs.length; i++){
-    console.log("ans",ans)
-        // flower","flow","flight"
-    for (let j =0; j<ans.length; j++){
-        // flow
-        console.log("ans",ans)
-        console.log("ans.length|,",ans.length)
-        console.log("strs",strs[i][j])
-        console.log("ans[j]",ans[j])
-        if(strs[i][j]==ans[j]){
-        res+=(ans[j]);
-        console.log("res",res)
-        } 
+var longestCommonPrefix = function (strs) {
+  let ans = strs[0],
+    res = "";
+  for (let i = 1; i < strs.length; i++) {
+    // flower","flow","flight"
+    for (let j = 0; j < ans.length; j++) {
+      if (strs[i][j] == ans[j]) {
+        res += ans[j];
+      }
     }
-        ans=res;
-            console.log(res)
-            res="";
-     
-            console.log("res",ans)
-   
-   
-
-    }
-    return ans
-    
+    ans = res;
+    res = "";
+  }
+  return ans;
 };
- strs = ["flower","flow","flight"] 
- console.log(longestCommonPrefix(strs))
+strs = ["flower", "flow", "flight"];
+console.log(longestCommonPrefix(strs));
